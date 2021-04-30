ef56c7bd8a93b9123b1f0db74fe4206dc83d1bd4
Wilson Neto B.R <contato@wilsonneto.com.br>
2021-04-29 21:43:14 -0300

    texts: changing the texts just for example

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React + CI/CD + Github Actions + Azure Blob Storage:<br />
+          Bora fazer um pipeline de deploy com GH Actions?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
